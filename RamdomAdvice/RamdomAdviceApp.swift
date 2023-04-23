4a8a895d5314caab8b5d063eaebd9e458cb7e836
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-22 20:19:03 -0400

    Initial Commit

---
+++ RamdomAdvice/RamdomAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamdomAdviceApp.swift
+//  RamdomAdvice
+//
+//  Created by Kiho Okazawa on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct RamdomAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
